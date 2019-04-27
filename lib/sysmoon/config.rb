f542d2e66e1b83d9cd88a3f109bb7861ac253773
Kiselev Valentine <mrexox@outlook.com>
2019-04-27 16:56:52 +0300

    Fixed linter issue

--- lib/sysmoon/config.rb
+++ lib/sysmoon/config.rb
@@ -4,27 +4,29 @@ require 'sysmoon/log'
 
 module Sysmoon
   module Config
-    def self.[](daemon)
-      read_config if needs_reading
+    class << self
+      def [](daemon)
+        read_config if needs_reading
 
-      @@config[daemon.to_s]
-    end
+        @@config[daemon.to_s]
+      end
 
-    private
+      private
 
-    def self.read_config
-      @@config = TomlRB.load_file(Constants::CONFIG_FILE)
-      # Setting unset defaults
-      @@config['sysmoond']['port'] ||= Constants::MOOND_PORT
-      @@config['sysdatad']['port'] ||= Constants::DATAD_PORT
-      @@config['syshand']['port']  ||= Constants::HAND_PORT
-      @@config['sync']['port']     ||= Constants::SYNC_PORT
-      @@config['discover_timeout'] ||= Constants::DISCOVER_TIMEOUT
-      Log.info("Config read: #{Constants::CONFIG_FILE}")
-    end
+      def read_config
+        @@config = TomlRB.load_file(Constants::CONFIG_FILE)
+        # Setting unset defaults
+        @@config['sysmoond']['port'] ||= Constants::MOOND_PORT
+        @@config['sysdatad']['port'] ||= Constants::DATAD_PORT
+        @@config['syshand']['port']  ||= Constants::HAND_PORT
+        @@config['sync']['port']     ||= Constants::SYNC_PORT
+        @@config['discover_timeout'] ||= Constants::DISCOVER_TIMEOUT
+        Log.info("Config read: #{Constants::CONFIG_FILE}")
+      end
 
-    def self.needs_reading
-      ! defined? @@config
+      def needs_reading
+        ! defined? @@config
+      end
     end
   end
 end
